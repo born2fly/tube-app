b0295e84a9045c35bee1a590d88daa0aeeee595f
Rich Leonard <rich@homeimprovement-li.com>
2017-03-07 13:58:15 -0500

    just started this...

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+
+
+$.ajax({
+		url: "https://www.googleapis.com/youtube/v3/search",
+		data: request,
+		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		type: "GET",
+	})
+
+
+/*	
+
+.done(function(result){ //this waits for the ajax to return with a succesful promise 
+		console.log(result);
+		var searchResults = showSearchResults(request.tagged, result.items.length);
+
+		$('.search-results').html(searchResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(result.items, function(i, item) {
+			var question = showQuestion(item);
+			$('.results').append(question);
+		});
+	})
+	
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+    
+*/
